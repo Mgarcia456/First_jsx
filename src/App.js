bc891b49f2668bb60f9e9c97531c54279b0ae409
Mgarcia456 <Asvp_miguel36@icloud.com>
2023-03-09 00:58:37 -0500

    first_jsx

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{fontSize: 60}}>Hello Dojo!</h1>
+      <h2 style={{fontSize: 30}}>Things I need to do:</h2>
+      <ul className="list-items">
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
     </div>
   );
 }
-
 export default App;
